8d06dc1a65f46d2fb2aef4e183c1a37bd565a5a8
Navneet Sharma <navneetsharma60761@gmail.com>
2025-02-16 11:04:36 +0000

    datatype summery done

---
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -0,0 +1,35 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 235649764654984756n 
+
+
+
+// Reference (Non primitive)
+
+// Array, Object, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myobj = {
+    name: "navneet",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof scoreValue);
