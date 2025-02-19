00db4a2b3545c3d4f9fc8deeb675571f27d5241f
Navneet Sharma <navneetsharma60761@gmail.com>
2025-02-19 10:13:27 +0000

    scope this and arrow

--- .devcontainer/03_basics/02_scopes.js
+++ .devcontainer/03_basics/02_scopes.js
@@ -15,7 +15,7 @@ if (true) {
 
 
 function one(){
-    const username = "hitesh"
+    const username = "navneet"
 
     function two(){
         const website = "youtube"
@@ -30,8 +30,8 @@ function one(){
 // one()
 
 if (true) {
-    const username = "hitesh"
-    if (username === "hitesh") {
+    const username = "navneet"
+    if (username === "navneet") {
         const website = " youtube"
         // console.log(username + website);
     }
