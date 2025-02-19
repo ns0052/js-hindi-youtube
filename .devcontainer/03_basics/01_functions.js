1907994c6b9b02c4d276c92fdb8e4c39e3b07015
Navneet Sharma <navneetsharma60761@gmail.com>
2025-02-19 04:18:32 +0000

    taking parameters in js

---
+++ .devcontainer/03_basics/01_functions.js
@@ -0,0 +1,71 @@
+
+function sayMyName(){
+    console.log("N");
+    console.log("A");
+    console.log("V");
+    console.log("N");
+    console.log("E");
+    console.log("E");
+    console.log("T");
+}
+
+// sayMyName()
+
+// function addTwoNumbers(number1, number2){
+
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2){
+
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+
+const result = addTwoNumbers(3, 5)
+
+// console.log("Result: ", result);
+
+
+function loginUserMessage(username = "sam"){
+    if(!username){
+        console.log("PLease enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("navneet"))
+// console.log(loginUserMessage("navneet"))
+
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "navneet",
+    prices: 299
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "Ankit",
+    price: 199
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
